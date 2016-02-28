9af5ad8b420e552199078a04a3336bed95ada0a1
caneslotocato <caneslotocato1@gmail.com>
2016-02-28 17:28:19 +0100

    Enemy script
    I do the enemy script whitout the variable for the energy(only the
    banishment cost, but you can use 0.). Remember to set the life with life
    > 0

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+    public LayerMask m_Mask;
+    public Vector2 target;
+    public float Speed;
+    public float life;
+    public float banishmentCost;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TakeTarget();
+        Move();
+        if (life <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void TakeTarget()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(), 0f, m_Mask, 0f, 2f);
+        if (hit.collider != null)
+        {
+
+            target = hit.collider.GetComponent<Tile>().GetNextTilePosition();
+            Debug.Log(target);
+        }
+    }
+    void Move()
+    {
+        transform.LookAt(target);
+        transform.Translate(Speed * transform.forward * Time.deltaTime);
+    }
+}
